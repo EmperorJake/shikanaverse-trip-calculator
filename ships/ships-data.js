2e22dc842ea71f379b68ccd1817391f572200284
EmperorJake <68182631+EmperorJake@users.noreply.github.com>
2026-09-13 15:29:01 +1000

    Add starship database template

---
+++ ships/ships-data.js
@@ -0,0 +1,21 @@
+/**
+ * Starships Database
+ * Add new starships by adding objects to this array
+ * Template:
+ * {
+ *     name: "Ship Name",
+ *     class: "Ship Class",
+ *     maxWarp: 5,
+ *     image: "ships/ship-name.png"
+ * }
+ */
+
+const STARSHIPS = [
+    {
+        name: "Placeholder Ship",
+        class: "Explorer",
+        maxWarp: 5,
+        image: "ships/placeholder.png"
+    }
+    // Add more starships below this line
+];
