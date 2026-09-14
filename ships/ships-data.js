a61026fb1a5a6c326fb7b22214ac03a9182d3554
Jakob Karl <jake.dander@gmail.com>
2026-09-14 14:18:10 +1000

    add starships

--- ships/ships-data.js
+++ ships/ships-data.js
@@ -12,10 +12,28 @@
 
 const STARSHIPS = [
     {
-        name: "Placeholder Ship",
-        class: "Explorer",
+        name: "Space Cruiser 1",
+        class: "Experimental",
         maxWarp: 5,
-        image: "ships/placeholder.png"
+        image: "ships/sg_sc1.png"
+    }
+    {
+        name: "PSY Courier Ship",
+        class: "Courier",
+        maxWarp: 20,
+        image: "ships/psy_courier_ship.png"
+    }
+    {
+        name: "PSY Breadbox",
+        class: "Freighter",
+        maxWarp: 40,
+        image: "ships/psy_breadbox.png"
+    }
+    {
+        name: "TSI General Mk1",
+        class: "Transport",
+        maxWarp: 50,
+        image: "ships/tsi_general_mk1.png"
     }
     // Add more starships below this line
 ];
